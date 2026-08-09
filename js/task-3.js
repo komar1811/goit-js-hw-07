68118eba98624000b6465348094095ac532da4e4
komar1811 <ostap.komar7@gmail.com>
2026-08-10 01:02:12 +0200

    fourth commit

--- js/task-3.js
+++ js/task-3.js
@@ -5,5 +5,7 @@ const name = document.querySelector("#name-output");
 input.addEventListener("input", (event) => {
   if (event.currentTarget.value.trim().length !== 0) {
     name.textContent = event.currentTarget.value.trim();
+  } else {
+    name.textContent = "Anonymous";
   }
 });
