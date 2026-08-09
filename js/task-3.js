46c735d715ee55074e1bf59368f210d358df2a95
komar1811 <ostap.komar7@gmail.com>
2026-08-10 00:58:49 +0200

    third commit

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ const input = document.querySelector("input#name-input");
 const name = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
-  if (event.currentTarget.value.trim() !== "") {
+  if (event.currentTarget.value.trim().length !== 0) {
     name.textContent = event.currentTarget.value.trim();
   }
 });
